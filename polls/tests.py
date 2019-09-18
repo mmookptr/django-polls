fbfc8f5732b97959f4146da103898ea777bbf2e1
Patteera Likitamnuayporn <mmookptr@Patteeras-MacBook-Pro.local>
2019-09-18 18:27:34 +0700

    fixed fail tests

--- polls/tests.py
+++ polls/tests.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
-from django.utils import reverse
+from django.urls  import reverse
 
 
 from .models import Question
@@ -18,6 +18,7 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
     def test_was_published_recently_with_old_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
@@ -37,13 +38,13 @@ class QuestionModelTests(TestCase):
         self.assertIs(recent_question.was_published_recently(), True)
 
 def create_question(question_text, days):
-"""
-Create a question with the given `question_text` and published the
-given number of `days` offset to now (negative for questions published
-in the past, positive for questions that have yet to be published).
-"""
-time = timezone.now() + datetime.timedelta(days=days)
-return Question.objects.create(question_text=question_text, pub_date=time)
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionIndexViewTests(TestCase):
@@ -102,6 +103,7 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
 class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
         """
--- polls/views.py
+++ polls/views.py
@@ -23,7 +23,7 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html'
+    template_name = 'polls/details.html'
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
@@ -42,7 +42,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
+        return render(request, 'polls/details.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
